a87f22b95d6fddd48430fe02acceb4d0dd19aa8f
Jo-R <jo.rigg8@gmail.com>
2020-12-31 18:05:38 +0000

    hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-b2981c52b23728af.d
@@ -0,0 +1,5 @@
+C:\Users\jorig\codingProjects\Rust\hello_cargo\target\debug\deps\hello_cargo-b2981c52b23728af.rmeta: src\main.rs
+
+C:\Users\jorig\codingProjects\Rust\hello_cargo\target\debug\deps\hello_cargo-b2981c52b23728af.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\jorig\codingProjects\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\jorig\codingProjects\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
