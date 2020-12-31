ad0ad7e57e59a487be8ba264566800934d7a55fc
Jo-R <jo.rigg8@gmail.com>
2020-12-31 18:32:50 +0000

    delete hello world

--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jorig\codingProjects\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
-
-C:\Users\jorig\codingProjects\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
-
-src\main.rs:
--- hello_cargo/target/rls/debug/deps/hello_cargo-b2981c52b23728af.d
+++
@@ -1,5 +0,0 @@
-c:\Users\jorig\codingProjects\Rust\hello_cargo\target\rls\debug\deps\hello_cargo-b2981c52b23728af.rmeta: src\main.rs
-
-c:\Users\jorig\codingProjects\Rust\hello_cargo\target\rls\debug\deps\hello_cargo-b2981c52b23728af.d: src\main.rs
-
-src\main.rs:
